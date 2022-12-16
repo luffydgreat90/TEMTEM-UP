081c098925451e8cdd271beeb2410848ad0b7e3d
marlon ansale <luffydgreat90@gmail.com>
2022-12-16 22:26:57 +0000

    Added Cache Image View

--- temtem/View/ImageView/ImageCacheView.swift
+++ temtem/View/ImageView/ImageCacheView.swift
@@ -6,17 +6,42 @@
 //
 
 import UIKit.UIImageView
+import Combine
 import TemtemFeed
 
 public final class ImageCacheView: UIImageView {
-    private let imageCacheService:ImageCacheService
+    private let imageCacheService: ImageCacheService
+    private let imageDataService: ImageDataService
+    private var cancellable = [AnyCancellable]()
    
-    init(withCache imageCacheService:ImageCacheService = ImageNSCacheService()) {
+    init(imageCacheService:ImageCacheService = ImageNSCacheService(), imageDataService:ImageDataService = ImageURLSessionDataService()) {
         self.imageCacheService = imageCacheService
+        self.imageDataService = imageDataService
         super.init(frame: .zero)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func loadImage(withURL url:URL){
+        if let data = try? imageCacheService.retrieve(dataForURL: url) {
+            self.setImage(image: UIImage(data: data))
+        }else{
+            imageDataService
+                .loadImage(withURL: url)
+                .receive(on: queueInitiated)
+                .sink(receiveCompletion: { _ in
+                    
+                }, receiveValue: { [weak self] data in
+                    try? self?.imageCacheService.insert(data, for: url)
+                    self?.setImage(image: UIImage(data: data))
+                }).store(in: &cancellable)
+
+        }
+    }
+    
+    deinit {
+        cancellable.first?.cancel()
     }
 }
