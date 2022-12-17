025172efea07f14e38f77b4defbd1eb44971f455
marlon ansale <luffydgreat90@gmail.com>
2022-12-17 16:32:19 +0000

    Added Testing for search.

--- temtemTests/ViewModels/TemtemListViewModelTests.swift
+++ temtemTests/ViewModels/TemtemListViewModelTests.swift
@@ -31,4 +31,34 @@ final class TemtemListViewModelTests: XCTestCase {
         sut.fetchTemtems()
         wait(for: [expectation], timeout:  1)
     }
+    
+    func test_searched_data_fetched() throws {
+        let sut = makeSUT()
+        let expectation = XCTestExpectation(description: "Search Temtem from Cache.")
+        
+        sut.$temtems.dropFirst(2)
+            .sink { temtems in
+                XCTAssertEqual(temtems.count, 1)
+                expectation.fulfill()
+        }.store(in: &cancellables)
+        
+        sut.fetchTemtems()
+        sut.searchTemtem(search: "Golz")
+        wait(for: [expectation], timeout:  1)
+    }
+    
+    func test_searched_none() throws {
+        let sut = makeSUT()
+        let expectation = XCTestExpectation(description: "No Temtem found.")
+        
+        sut.$temtems.dropFirst(2)
+            .sink { temtems in
+                XCTAssertEqual(temtems.count, 0)
+                expectation.fulfill()
+        }.store(in: &cancellables)
+        
+        sut.fetchTemtems()
+        sut.searchTemtem(search: "none")
+        wait(for: [expectation], timeout:  1)
+    }
 }
