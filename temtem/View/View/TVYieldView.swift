31de895529b30bb353f500e53713fc4cb49edd18
marlon ansale <luffydgreat90@gmail.com>
2022-12-20 19:06:54 +0000

    Added yield view.

--- temtem/View/View/TVYieldView.swift
+++ temtem/View/View/TVYieldView.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import UIKit
+import TemtemFeed
+
+class TVYieldView: UIView {
+    
+    public private(set) var yieldLabel: UILabel = {
+        let yieldLabel = UILabel()
+        yieldLabel.text = "TV Yield"
+        yieldLabel.font = .systemFont(ofSize: 17, weight: .black)
+        return yieldLabel
+    }()
+    
+    public private(set) var yieldStack: UIStackView = {
+        let yieldStack = UIStackView()
+        yieldStack.axis = .horizontal
+        yieldStack.distribution = .fillEqually
+        yieldStack.spacing = 8
+        yieldStack.translatesAutoresizingMaskIntoConstraints = false
+        return yieldStack
+    }()
+    
+    public init(){
+        super.init(frame: .zero)
+        self.setupUI()
+        self.setupAutoLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        self.addSubview(yieldLabel)
+        self.addSubview(yieldStack)
+    }
+    
+    private func setupAutoLayout(){
+        NSLayoutConstraint.activate([
+            yieldLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            yieldLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            yieldStack.topAnchor.constraint(equalTo: yieldLabel.bottomAnchor, constant: 16),
+            yieldStack.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            yieldStack.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    public func setupTraits(yields:[TVYields:Int]){
+        yields.enumerated().forEach { (yield,num) in
+            let label = UILabel()
+            label.font = .systemFont(ofSize: 16, weight: .bold)
+            label.text = "\(yield.description) \(num)"
+            yieldStack.addArrangedSubview(label)
+        }
+    }
+    
+}
--- temtem/View/View/TraitsStackView.swift
+++ temtem/View/View/TraitsStackView.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import UIKit.UIStackView
+import UIKit
 
 public final class TraitsView: UIView{
     
